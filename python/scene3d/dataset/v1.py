d60235605065b2a1e67a1c4e041728e2d8b0b663
Daeyun Shin <daeyunshin@gmail.com>
2018-05-10 20:45:29 -0700

    Add Unet-based model. Change how log depth is computed for multi-layer.

--- python/scene3d/dataset/v1.py
+++ python/scene3d/dataset/v1.py
@@ -5,12 +5,11 @@ from scene3d import config
 from scene3d import io_utils
 from scene3d import log
 from os import path
-import imageio
 import cv2
 
 
 class MultiLayerDepth(data.Dataset):
-    def __init__(self, first_n=None, train=True):
+    def __init__(self, first_n=None, train=True, input_scale=1):
         self.filename_prefixes = io_utils.read_lines_and_strip(path.join(config.scene3d_root, 'v1/renderings.txt'))
 
         if train:
@@ -20,11 +19,12 @@ class MultiLayerDepth(data.Dataset):
             train_first_n = int(len(self.filename_prefixes) * 0.95)
             self.filename_prefixes = self.filename_prefixes[train_first_n:]
 
-        if first_n is not None:
+        if first_n is not None and first_n > 0:
             self.filename_prefixes = self.filename_prefixes[:first_n]
 
         self.input_image_size = (480, 640)
         self.rgb_mean = np.array([178.17808226, 158.50391329, 142.51412396], dtype=np.float32)
+        self.input_scale = input_scale
 
     def __len__(self):
         return len(self.filename_prefixes)
@@ -46,6 +46,9 @@ class MultiLayerDepth(data.Dataset):
         in_rgb = cv2.cvtColor(in_bgr, cv2.COLOR_BGR2RGB)
         in_rgb = in_rgb.transpose([2, 0, 1]).astype(np.float32)
         in_rgb -= self.rgb_mean[:, None, None]
+
+        if self.input_scale != 1:
+            in_rgb *= self.input_scale
 
         # NOTE: target_depth contains nan values. They need to be replaced or excluded when computing the loss function.
         return example_name, in_rgb, target_depth
