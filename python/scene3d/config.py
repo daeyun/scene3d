c7c2d36ce957f1a7b2306fa164c724cbba5c75c7
Zhile Ren <jrenzhile@gmail.com>
2018-07-17 15:07:24 -0700

    config.py

--- python/scene3d/config.py
+++ python/scene3d/config.py
@@ -1,11 +1,25 @@
 from os import path
+import socket
 
-suncg_root = '/data2/suncg_data'
+cur_hostname = socket.gethostname()
+if (cur_hostname=='ren-ubuntu')
+    suncg_root = '/media/ren/devRen4T/research/datasets/suncg'
 
-# Directory containing PBRS data.
-# Subdirectories include: camera_v2, mlt_v2, ...
-pbrs_root = '/data2/pbrs'
+    # Directory containing PBRS data.
+    # Subdirectories include: camera_v2, mlt_v2, ...
+    pbrs_root = '/media/ren/devRen4T/research/datasets/pbrs'
 
-scene3d_root = '/data2/scene3d'
+    scene3d_root = '/media/ren/devRen4T/research/datasets/scene3d'
 
-cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
+    cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
+
+else:
+    suncg_root = '/data2/suncg_data'
+
+    # Directory containing PBRS data.
+    # Subdirectories include: camera_v2, mlt_v2, ...
+    pbrs_root = '/data2/pbrs'
+
+    scene3d_root = '/data2/scene3d'
+
+    cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
