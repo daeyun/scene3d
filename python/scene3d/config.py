bbd38359a4e6ba3ec7ba49c08a3e4b4c6af69890
Daeyun Shin <daeyunshin@gmail.com>
2018-09-17 23:11:40 -0700

    Update config

--- python/scene3d/config.py
+++ python/scene3d/config.py
@@ -1,25 +1,50 @@
 from os import path
 import socket
 
+"""
+pbrs_root:
+    Directory containing PBRS data.
+    Subdirectories include: camera_v2, mlt_v2, ...
+suncg_root:
+    Directory containing SUNCG data.
+    Subdirectories include: house, object, room, ...
+scene3d:
+    This project's main data directory. May contain symlinks.
+    Subdirectories include: v2, v8/renderings, ...
+"""
+
 hostname = socket.gethostname()
-if hostname == 'ren-ubuntu':
+if hostname == 'daeyun-01':
+    suncg_root = '/data/daeyun-data-02/suncg_data'
+    pbrs_root = '/data/daeyun-data-02/data2/pbrs'
+    scene3d_root = '/data/daeyun-data-02/scene3d'
+
+elif hostname == 'daeyun-02':
+    suncg_root = '/data/daeyun-data-01/suncg_data'
+    pbrs_root = '/data/daeyun-data-01/data2/pbrs'
+    scene3d_root = '/data/daeyun-data-01/scene3d'
+
+elif hostname == 'daeyun-03':
+    suncg_root = '/data/daeyun-data-03/suncg_data'
+    pbrs_root = '/data/daeyun-data-03/data2/pbrs'
+    scene3d_root = '/data/daeyun-data-03/scene3d'
+
+elif hostname == 'ren-ubuntu':
     suncg_root = '/media/ren/devRen4T/research/datasets/suncg'
-
-    # Directory containing PBRS data.
-    # Subdirectories include: camera_v2, mlt_v2, ...
     pbrs_root = '/media/ren/devRen4T/research/datasets/pbrs'
-
     scene3d_root = '/media/ren/devRen4T/research/datasets/scene3d'
 
-    cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
-
-else:
+elif hostname == 'daeyun-vision-lab':
     suncg_root = '/data2/suncg_data'
-
-    # Directory containing PBRS data.
-    # Subdirectories include: camera_v2, mlt_v2, ...
     pbrs_root = '/data2/pbrs'
-
     scene3d_root = '/data2/scene3d'
 
-    cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
+elif path.isfile(path.expanduser('~/.is_uci_vision_cluster')):
+    suncg_root = '/home/daeyuns/data2/suncg_data'
+    pbrs_root = '/home/daeyuns/data2/pbrs'
+    scene3d_root = '/home/daeyuns/data2/scene3d'
+
+else:
+    raise RuntimeError('Unknown hostname: {}'.format(hostname))
+
+cpp_third_party_root = path.abspath(path.join(path.dirname(__file__), '../../cpp/third_party'))
